932f3d11c0354c4b2b7c9bcc6504c22407a1234b
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-12-01 18:16:01 +0100

    add some telegram commands to Parsivel class

--- disdrodl/main.py
+++ disdrodl/main.py
@@ -3,9 +3,8 @@
 import csv
 import time
 import serial
-import logging
 
-parsivel = serial.Serial(Variable.PORT, Variable.PARSIVEL_BAUDRATE, timeout=1)  # Defines the serial port
+parsivel = serial.Serial('/dev/ttyUSB0', baudrate=19200, timeout=1)  # Defines the serial port
 parsivel.reset_input_buffer()  # Flushes input buffer
 
 Parsivel_name = 'PAR002'
@@ -16,7 +15,7 @@ parsivel_request_field_90 = 'CS/R/90\r'  # Asks the Parsivel to read out field 6
 parsivel_command_list = 'CS/?\r'  # Reads out a list of serial commands for the Parsivel.
 parsivel_ott_telegram = 'CS/M/M/0\r'  # The Parsivel broadcasts the OTT telegram.
 parsivel_telegram_start = 'CS/*/D/0\r'  # The Parsivel broadcasts the OTT telegram.
-parsivel_user_telegram = 'CS/M/M/1\r'  # The Parsivel broadcasts the user defined telegram.
+# parsivel_user_telegram = 'CS/M/M/1\r'  # The Parsivel broadcasts the user defined telegram.
 parsivel_set_telegram_list = 'CS/M/S/%01;%02;%03;%04;%05;%06;%07;%08;%09;%10;%11;%12;%13;%14;%15;%16;%17;%18;%19;%20;%21;%22;%23;%24;%25;%26;%27;%28;%30;%31;%32;%33;%34;%35;%60;%90;%91;%93\r'  # Defines which fields are in the telegram
 parsivel_current_configuration = 'CS/L\r'  # Outputs current configuration
 parsivel_impulse_mode = 'CS/I/60\r'  # Turns poll mode off
@@ -25,9 +24,9 @@ parsivel_set_date = 'CS/D/' + time.strftime("%d.%m.%Y") + '\r'  # Sets the date 
 parsivel_set_name = 'CS/K/' + Parsivel_name + '\r'  # Sets the name of the Parsivel, maximum 10 characters
 parsivel_set_ID = 'CS/J/' + Parsivel_ID + '\r'  # Sets the ID of the Parsivel, maximum 4 numerical characters
 parsivel_reset_factory_settings = 'CS/F/1\r'  # Resets the Parsivel to factory settings.
-parsivel_real_time = 'CS/U\r'
-parsivel_set_real_time = 'CS/U/' + time.strftime("%d.%m.%Y ") + time.strftime("%H:%M:%S") + '\r'
-parsivel_restart = 'CS/Z/1\r'
+# parsivel_real_time = 'CS/U\r'
+# parsivel_set_real_time = 'CS/U/' + time.strftime("%d.%m.%Y ") + time.strftime("%H:%M:%S") + '\r'
+# parsivel_restart = 'CS/Z/1\r'
 
 # parsivel.write(parsivel_ott_telegram.encode('utf-8')) # Writes the Parsivel OTT telegram command to the Parsivel
 # parsivel.write(parsivel_set_telegram_list.encode('utf-8')) # Writes the parsivel user telegram string to the Parsivel
--- disdrodl/parsivel.py
+++ disdrodl/parsivel.py
@@ -1,4 +1,5 @@
 from .disdrometer import Disdrometer
+import time
 
 
 class Parsivel(Disdrometer):
@@ -6,4 +7,17 @@ class Parsivel(Disdrometer):
         super().__init__()
 
     def configure(self):
-        self.ser.readline()
+        pass
+
+    # TODO: ask Rob details
+    def ask_real_time(self, command='CS/U\r'):
+        self.ser.write(command.encode('utf-8'))
+
+    def set_real_time(self, command=f'CS/U/{time.strftime("%d.%m.%Y %H:%M:%S")}\r'):
+        self.ser.write(command.encode('utf-8'))
+
+    def restart(self, command='CS/Z/1\r'):
+        self.ser.write(command.encode('utf-8'))
+
+    def ask_user_telegram(self, command='CS/M/M/1\r'):
+        self.ser.write(command.encode('utf-8'))
