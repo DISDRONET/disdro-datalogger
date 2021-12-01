59057aadcafefab199f62b76544fe72359f96e57
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-12-01 15:10:58 +0100

    define dummy Parsivel constructor

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM balenalib/%%BALENA_MACHINE_NAME%%-python:3.6-buster
+
--- disdrodl/parsivel.py
+++ disdrodl/parsivel.py
@@ -1,2 +1,25 @@
-class Parsivel:
-    pass
+import serial
+import time
+# import sys
+
+BAUD=19200
+
+#TODO ask Rob if this is the right timeout
+READ_DELAY=1
+
+#TODO maybe create a class "Disdrometer" that contains all the functions
+# common to all the parsivels and thies devices
+
+# define a Parsivel class
+class Parsivel(object):
+    """
+    Initialize the device
+    :param device: /dev/yourConsoleDevice (e.g. /dev/ttyUSB0)
+    """
+    def __init__(self, device):
+        self.ser = serial.Serial(device, BAUD, timeout=READ_DELAY)
+    
+    # define a function to connect to the serial port
+    def readline(self):
+        self.ser.readline()
+        
